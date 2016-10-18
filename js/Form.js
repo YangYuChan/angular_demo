ee2af621d973d0bdbff873ba93519b56f149e0a9
YangYuChan <yuchan_yang@126.com>
2016-10-18 11:59:02 +0800

    Signed-off-by: YangYuChan <yuchan_yang@126.com>

---
+++ js/Form.js
@@ -0,0 +1,26 @@
+var userInfoModule= angular.module("UserInfoModule",[]);
+userInfoModule.controller("UserInfoCtrl",["$scope",function($scope){
+	$scope.userInfo={
+		email:"646732184@qq.com",
+		password:"123456",
+		autoLogin:true
+	};
+	$scope.getFormData=function(){
+		console.log($scope.userInfo);
+	};
+	$scope.setFormData=function(){
+		$scope.userInfo={
+		email:"yuchan_yang@126.com",
+		password:"123456789",
+		autoLogin:false
+	};
+	}
+	$scope.resetFormData=function(){
+		$scope.userInfo={
+		email:"646732184@qq.com",
+		password:"123456",
+		autoLogin:true
+	};
+	}
+}
+])
